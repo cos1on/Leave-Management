e27a608bf71b0c791926311967d31040c728e073
Umut Olcay <uolcay_1907@outlook.com>
2020-11-29 15:33:52 +0300

    Added

--- Leave-Management/Contracts/ILeaveTypeRepository.cs
+++ Leave-Management/Contracts/ILeaveTypeRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Leave_Management.Contracts
 {
-    interface ILeaveTypeRepository : IRepositoryBase<LeaveType>
+    public interface ILeaveTypeRepository : IRepositoryBase<LeaveType>
     {
 
         ICollection<LeaveType> GetEmployeesByLeaveType(int id);
